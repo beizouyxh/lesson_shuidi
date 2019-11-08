b7910598809e83c6f10c319dbbb0b0b5b1e96d5a
13033229336 <874609947@qq.com>
2019-11-08 22:03:32 +0800

    turing

---
+++ node/turing/app.js
@@ -0,0 +1,19 @@
+const TULING= require('tuling');
+
+const aiTuling =new TULING({
+    key:'40e774ff3d1d4798aa9fb84c53151aa3'
+
+});
+//立即执行函数
+(async()=>{
+    //机器人
+   // Console.log('arrow function') 
+   const result = await aiTuling.send({
+       userid: 1,
+       info:'今天的天气',
+       loc: '南昌市'
+   });
+   console.log(result);
+})() 
+//console.log(func);
+//func();
