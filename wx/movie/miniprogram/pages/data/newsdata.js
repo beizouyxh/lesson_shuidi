e7139c823d377e550a8680157f39f8126b0b567a
13033229336 <874609947@qq.com>
2020-02-25 21:52:31 +0800

    wxmovie news数据绑定

---
+++ wx/movie/miniprogram/pages/data/newsdata.js
@@ -0,0 +1,15 @@
+var initData = [{
+        "newsid": 0,
+        "authoricon": "../../images/author.jpg",
+        "authorName": "北走",
+        "authorDate": "2020/2/24",
+        "title": "岁月不老，你我不散",
+        "articleImg": "../../images/view4.png",
+        "articleText": "岁月不老，你我不散岁月不老，你我不散岁月不老，你我不散岁月不老，你我不散岁月不老，你我不散",
+        "articleLikeComment": "30",
+        "articleLikeSC": "12"
+    }]
+    //导出可以被外部访问
+module.exports = {
+    initData: initData
+}
--- wx/movie/miniprogram/pages/news/news.js
+++ wx/movie/miniprogram/pages/news/news.js
@@ -1,10 +1,17 @@
+var newsData = require("../data/newsdata.js");
 Page({
     data: {
         indicatorDots: true,
         autoPlay: true,
         interval: 2000,
-        circular: true
-
-
+        circular: true,
+        useData: ""
+    },
+    onLoad: function(options) { //页面周期函数
+        //页面初始化  options为页面跳转所带来的参数
+        // console.log(newData);
+        this.setData({ //this.setData可以让view重绘
+            useData: newsData.initData
+        })
     }
 })
