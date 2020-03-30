24d737f524c29bac20e2e3494dc6c72774fa676a
13033229336 <874609947@qq.com>
2020-03-30 21:56:44 +0800

    圆圈中最后的数字  leetcode

---
+++ leetcode/圆圈中最后的数字/index.js
@@ -0,0 +1,8 @@
+var lastRemaining = function(n, m) {
+    let ans = 0;
+    for (let i = 2; i <= n; i++) {
+        ans = (ans + m) % i;
+    }
+    return ans;
+};
+console.log(lastRemaining(100, 3));
