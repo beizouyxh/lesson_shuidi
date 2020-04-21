fe0d5a2c22efd916afee2e71e97a0883257c3eab
13033229336 <874609947@qq.com>
2020-04-21 19:31:19 +0800

    vue-project store index,actions

---
+++ vue/vue-project/git-admin-master/git-admin-fronted/src/store/index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import { classActions, studentActions, userActions } from "./actions";
+import { classMutations, studentMutations, userMutations } from "./mutations";
+import { classGetters, studentGetters, userGetters } from "./getters";
+import { getToken } from "../utils/auth";
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        classList: [],
+        classInfo: {},
+        studentList: [],
+        stuTotal: 0,
+        studentInfo: {},
+        curStuCommits: [],
+        token: getToken()
+    },
+    mutations: {
+        ...classMutations,
+        ...studentMutations,
+        ...userMutations
+    },
+    actions: {
+        ...classActions,
+        ...studentActions,
+        ...userActions
+    },
+    getters: {
+        ...classGetters,
+        ...studentGetters,
+        ...userGetters
+    }
+});
