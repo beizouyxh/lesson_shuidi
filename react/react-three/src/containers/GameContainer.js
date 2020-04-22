11f8988aade5055184706d6c35f39ea0f9cb2faf
13033229336 <874609947@qq.com>
2020-04-22 21:39:38 +0800

    containers2.js

--- react/react-three/src/containers/GameContainer.js
+++ react/react-three/src/containers/GameContainer.js
@@ -8,17 +8,30 @@ export default class GameContainer extends Component {
     super(props, context)
     this.cameraPositon = new THREE.Vector3(0, 0, 5) //三维空间
     this.state = {
+      // 运动  几何体 x, y z 
+      cubeRotation: new THREE.Euler() // mvvm 
+    }
+    this._onAnimate = () => {
+      this.setState({
+        cubeRotation: new THREE.Euler(
+          this.state.cubeRotation.x + 0.1,
+          this.state.cubeRotation.y + 0.1,
+          0
+        )
+      })
     }
   }
   render() {
     const width = window.innerWidth,
       height = window.innerHeight
-   
+    // console.log(width, height)
     return (
       <React3
         mainCamera="camera"
         width={width}
-        height={height}>
+        height={height}
+        onAnimate={this._onAnimate}
+        >
         <scene>
           <perspectiveCamera 
             name="camera"
@@ -28,7 +41,9 @@ export default class GameContainer extends Component {
             far={1000}
             position={this.cameraPositon}
           />
-          <mesh>
+          <mesh 
+           rotation={this.state.cubeRotation}
+          >
             <boxGeometry
             width={1}
             height={1}
