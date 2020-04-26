e128a01f7b0ce73d96b6a0da6af0f00e8284333a
13033229336 <874609947@qq.com>
2020-04-26 16:21:37 +0800

    vue-project 前端

---
+++ vue/vue-project/git-admin-master/git-admin-fronted/src/router/index.js
@@ -0,0 +1,95 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import StudnetList from "../views/StudnetList.vue";
+
+Vue.use(VueRouter);
+
+const routes = [{
+        path: "/",
+        redirect: "/student/list"
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () =>
+            import ("../views/Login.vue")
+    },
+    {
+        path: "/student",
+        name: "Home",
+        component: Home,
+        children: [{
+                path: "list",
+                name: "StudentList",
+                component: StudnetList
+            },
+            {
+                path: "add",
+                name: "Add",
+                component: () =>
+                    import ("../views/AddStudent.vue")
+            },
+            {
+                path: "edit/:id",
+                name: "EditStu",
+                component: () =>
+                    import ("../views/EditStudent.vue")
+            },
+            {
+                path: "detail/:id",
+                name: "StuDetail",
+                component: () =>
+                    import ("../views/StuDetail.vue")
+            }
+        ]
+    },
+    {
+        path: "/class",
+        name: "Class",
+        component: Home,
+        children: [{
+                path: "list",
+                name: "ClassList",
+                component: () =>
+                    import ("../views/ClassList.vue")
+            },
+            {
+                path: "add",
+                name: "AddClass",
+                component: () =>
+                    import ("../views/AddClass.vue")
+            },
+            {
+                path: "edit/:id",
+                name: "EditClass",
+                component: () =>
+                    import ("../views/EditClass.vue")
+            },
+            {
+                path: "detail/:name",
+                name: "ClassDetail",
+                component: () =>
+                    import ("../views/ClassDetail.vue")
+            }
+        ]
+    },
+    {
+        path: "/404",
+        name: "404",
+        component: () =>
+            import ("../views/404.vue")
+    },
+    {
+        path: "*",
+        redirect: "/404"
+    }
+];
+
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
+});
+
+export default router;
