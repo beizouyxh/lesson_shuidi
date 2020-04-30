a9acd6271d00643066fedc22cb657ccb13581563
13033229336 <874609947@qq.com>
2020-04-30 19:36:05 +0800

    导航

--- HeiMa/pages/index/index.js
+++ HeiMa/pages/index/index.js
@@ -1,8 +1,21 @@
+// import { request } from "express"
+
 Page({
   data:{
         indicatorDots: true,
         autoPlay: true,
         interval: 2000,
         circular: true,
-  }
+  },
+  // onLoad: function(options){
+  //  this.getCateList();
+  // },
+  // getCateList(){
+  //   request({ url :"https//api.zbztb.cn/api/public/vi/home/catitems"})
+  //   .then(result =>{
+  //     this.setData({
+  //       cateList:result.data.message
+  //     })
+  //   })
+  // },
 })
