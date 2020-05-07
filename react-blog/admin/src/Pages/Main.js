ddf4b570599e8eea58af31488525442d77539ed9
13033229336 <874609947@qq.com>
2020-05-07 22:01:44 +0800

    后台主页

--- react-blog/admin/src/Pages/Main.js
+++ react-blog/admin/src/Pages/Main.js
@@ -1,13 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./Login";
+import AdminIndex from './AdminIndex'
 
-import React from 'react';
-import { BrowserRouter as Router,Route} from "react-router-dom";
-
-import Login from './Login'
-function Main(){
-    return (
-        <Router>      
-            <Route path="/login/" exact component ={Login} />
-        </Router>
-    )
+function Main() {
+  return (
+    <Router>
+    
+        <Route path="/login/" exact component={Login}></Route>
+        <Route path="/index/" exact component={AdminIndex} />
+     
+    </Router>
+  );
 }
+
 export default Main
