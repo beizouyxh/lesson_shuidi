9f6d3f52533633127d07bc07d0a2ca8763ffab24
13033229336 <874609947@qq.com>
2020-05-08 17:28:33 +0800

    blog 添加文章页面制作

--- react-blog/admin/src/Pages/AdminIndex.js
+++ react-blog/admin/src/Pages/AdminIndex.js
@@ -8,6 +8,8 @@ import {
     ProfileOutlined,
     SmileOutlined,
   } from "@ant-design/icons";
+  import {Route} from 'react-router-dom'
+  import AddArticle from './AddArticle'
 
 const {  Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
@@ -58,52 +60,17 @@ function AdminIndex(){
           <Breadcrumb.Item>后台管理</Breadcrumb.Item>
           <Breadcrumb.Item>工作台</Breadcrumb.Item>
         </Breadcrumb>
-        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>博客工作台.</div>
+        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+            <div>
+              <Route path="/index/" exact  component={AddArticle} />
+            </div>
+            
+        </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>JSPang.com</Footer>
     </Layout>
   </Layout>
 )
-
-//   return (
-//     <Layout style={{ minHeight: '100vh' }}>
-//       <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-//         <div className="logo" />
-//         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-//           <Menu.Item key="1" icon={<PieChartOutlined />}>
-//             Option 1
-//           </Menu.Item>
-//           <Menu.Item key="2" icon={<DesktopOutlined />}>
-//             Option 2
-//           </Menu.Item>
-//           <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-//             <Menu.Item key="3">Tom</Menu.Item>
-//             <Menu.Item key="4">Bill</Menu.Item>
-//             <Menu.Item key="5">Alex</Menu.Item>
-//           </SubMenu>
-//           <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-//             <Menu.Item key="6">Team 1</Menu.Item>
-//             <Menu.Item key="8">Team 2</Menu.Item>
-//           </SubMenu>
-//           <Menu.Item key="9" icon={<FileOutlined />} />
-//         </Menu>
-//       </Sider>
-//       <Layout className="site-layout">
-//         <Header className="site-layout-background" style={{ padding: 0 }} />
-//         <Content style={{ margin: '0 16px' }}>
-//           <Breadcrumb style={{ margin: '16px 0' }}>
-//             <Breadcrumb.Item>User</Breadcrumb.Item>
-//             <Breadcrumb.Item>Bill</Breadcrumb.Item>
-//           </Breadcrumb>
-//           <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-//             Bill is a cat.
-//           </div>
-//         </Content>
-//         <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-//       </Layout>
-//     </Layout>
-//  );
-
 }
 
 export default AdminIndex
