16ebdfdd457933bfe1f8dfd95f810efb5f396f95
13033229336 <874609947@qq.com>
2020-05-16 21:10:09 +0800

    404

---
+++ react/react-expriment/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Comcurrent from './Concurrent'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Comcurrent></Comcurrent>
+    </div>
+  );
+}
+
+export default App;
