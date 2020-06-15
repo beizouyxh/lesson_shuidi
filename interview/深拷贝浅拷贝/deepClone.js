edd9283e8b1ea52a0a190580f30a4b5e6d0f9ca3
13033229336 <874609947@qq.com>
2020-06-15 23:47:45 +0800

    深拷贝

---
+++ interview/深拷贝浅拷贝/deepClone.js
@@ -0,0 +1,53 @@
+const target = {
+    field1: 1,
+    field2: undefined,
+    field3: {
+        child: 'child'
+    },
+    field4: [2, 4, 8],
+    f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: {} } } } } } } } } } } },
+};
+
+target.target = target;
+
+// console.time();
+// const result = clone(target);
+// console.timeEnd();
+
+// console.time();
+// const result2 = clone(target);
+// console.timeEnd();
+
+function clone(target, map = new WeakMap()) {
+    if (typeof target === 'object') {
+        const isArray = Array.isArray(target);
+        let cloneTarget = isArray ? [] : {};
+
+        if (map.get(target)) {
+            return map.get(target);
+        }
+        map.set(target, cloneTarget);
+
+        const keys = isArray ? undefined : Object.keys(target);
+        forEach(keys || target, (value, key) => {
+            if (keys) {
+                key = value;
+            }
+            cloneTarget[key] = clone(target[key], map);
+        });
+
+        return cloneTarget;
+    } else {
+        return target;
+    }
+}
+function forEach(array, iteratee) {
+    let index = -1;
+    const length = array.length;
+    while (++index < length) {
+        iteratee(array[index], index);
+    }
+    return array;
+}
+
+console.log(clone(target))
