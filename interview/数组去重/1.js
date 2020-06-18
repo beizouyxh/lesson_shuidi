f7d0cf20efd97f42ca70d93d3227ef472e0f61c8
13033229336 <874609947@qq.com>
2020-06-18 17:17:28 +0800

    去重

--- interview/数组去重/1.js
+++ interview/数组去重/1.js
@@ -1,28 +1,29 @@
 var arr=[1,3,5,6,3,5];
-// const arr2=new Set(arr)
-// console.log(arr2)
+
+const arr2=new Set(arr)
+console.log(arr2)
 
 
-// function unique(arr){
-//     let res=[]
-//     for(let i=0;i<arr.length;i++){
-//         if(!res.includes(arr[i])){
-//            res.push(arr[i])
-//         }
-//     }
-//     return res
-// }
-// onsole.log(unique(arr));
+function unique(arr){
+    let res=[]
+    for(let i=0;i<arr.length;i++){
+        if(!res.includes(arr[i])){
+           res.push(arr[i])
+        }
+    }
+    return res
+}
+onsole.log(unique(arr));
 
 
-// let res=[]
-// for(let i=0;i<arr.length;i++){
-//     if(res.indexOf(arr[i])==-1){
-//         res.push(arr[i])
-//     }
+let res=[]
+for(let i=0;i<arr.length;i++){
+    if(res.indexOf(arr[i])==-1){
+        res.push(arr[i])
+    }
   
-// }
-// console.log(res)
+}
+console.log(res)
 
 let len=arr.length
 for(let i=0;i<len;i++){
@@ -35,3 +36,10 @@ for(let i=0;i<len;i++){
     }
 }
 console.log(arr)
+
+
+function unique(array) {
+    return Array.from(new Set(array));
+  }
+  
+  unique([1, 1, 2, 3, 3]); // => [1, 2, 3]
